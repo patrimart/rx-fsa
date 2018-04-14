df27de51c1981e76162ec4ec636898c12ead9239
Patrick Martin <bypatrickmartin@gmail.com>
2018-04-14 00:58:35 -0700

    Meta type cleanup

--- src/rxjs/utils.ts
+++ src/rxjs/utils.ts
@@ -1,4 +1,6 @@
 import { Meta } from "../actions/interfaces";
 
-export const actionChainer = (type: string) => <M extends Meta>(meta: M): M =>
-  Object.assign({}, meta, { $$typeChain: [...meta.$$typeChain, type] });
+export const actionChainer = (type: string) => <M extends Meta>(meta: M): M => ({
+  ...(meta as any),
+  $$typeChain: [...(meta.$$typeChain || []), type],
+});
