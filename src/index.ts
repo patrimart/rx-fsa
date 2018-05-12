96e0ab5cb9d5c5454cf0831f41273d359194c0dc
Patrick Martin <bypatrickmartin@gmail.com>
2018-05-12 01:11:47 -0700

    Improve docs. ngrx tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./actions/";
+export * from "./ngrx";
 export * from "./reducers/";
 export * from "./rxjs/";
--- src/ngrx/lazySelect.ts
+++ src/ngrx/lazySelect.ts
@@ -3,24 +3,47 @@ import { filter, map } from "rxjs/operators";
 
 import { MemoizedSelector, select, Store } from "@ngrx/store";
 
-import { Action, ActionCreator, Meta } from "../actions/";
+import { Action, Meta } from "../actions/";
 
 /**
- * Description
+ * Builds a MemoizedSelector that, if the predicate fails, will dispatch action(s)
+ * to ultimately satisfy the predicate and emit a value.
  */
-export const lazySelectFactory = <S, R, V, P = any, M extends Meta = any>(
-  selector: (v: V) => MemoizedSelector<S, R>,
-  actions: (v: V) => Array<Action<P, M>>,
+export function lazySelectFactory<S, R, P = any, M extends Meta = any>(
+  selector: MemoizedSelector<S, R>,
+  action: Action<P, M>,
+  predicate?: (r: R | null | undefined) => boolean,
+): (store$: Store<S>) => () => (source: Observable<S>) => Observable<R>;
+
+export function lazySelectFactory<S, R, P = any, M extends Meta = any>(
+  selector: () => MemoizedSelector<S, R>,
+  actions: () => Array<Action<P, M>>,
+  predicate?: (r: R | null | undefined) => boolean,
+): (store$: Store<S>) => () => (source: Observable<S>) => Observable<R>;
+
+export function lazySelectFactory<S, R, V, P = any, M extends Meta = any>(
+  selector: (() => MemoizedSelector<S, R>) | ((v: V) => MemoizedSelector<S, R>),
+  actions: (() => Array<Action<P, M>>) | ((v: V) => Array<Action<P, M>>),
+  predicate?: (r: R | null | undefined) => boolean,
+): (store$: Store<S>) => (v: V) => (source: Observable<S>) => Observable<R>;
+
+export function lazySelectFactory<S, R, V, P = any, M extends Meta = any>(
+  selector: ((v?: V) => MemoizedSelector<S, R>) | MemoizedSelector<S, R>,
+  actions: ((v?: V) => Array<Action<P, M>>) | Action<P, M>,
   predicate = (r: R | null | undefined) => !!r,
-) => (store$: Store<S>) => (v: V) => (source: Observable<S>) =>
-  source.pipe(
-    select(selector(v)),
-    map((result: R) => {
-      if (!predicate(result)) {
-        actions(v).forEach(a => store$.dispatch(a));
-        return undefined;
-      }
-      return result;
-    }),
-    filter((obj: R | undefined): obj is R => obj !== undefined),
-  );
+) {
+  const selectorFn = "projector" in selector ? (_?: V) => selector : selector;
+  const actionsFn = typeof actions === "function" ? actions : (_?: V) => [actions];
+  return (store$: Store<S>) => (v?: V) => (source: Observable<S>) =>
+    source.pipe(
+      select(selectorFn(v)),
+      map((result: R) => {
+        if (!predicate(result)) {
+          actionsFn(v).forEach(a => store$.dispatch(a));
+          return undefined;
+        }
+        return result;
+      }),
+      filter((obj: R | undefined): obj is R => obj !== undefined),
+    );
+}
